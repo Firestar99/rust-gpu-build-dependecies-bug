d0eb701e68444bc82723c6d8c5bc75164188fa6b
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2023-01-19 20:51:55 +0100

    broken: another attempt at fixing circular

--- engine/build.rs
+++ engine/build.rs
@@ -1,11 +1,18 @@
+use std::env;
 use std::error::Error;
 use std::path::PathBuf;
 
 use spirv_builder::{MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
+	let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+	if target_arch == "spirv" {
+		println!("exiting successfully to prevent recursion: target arch is spir-v");
+		return Ok(());
+	}
+
 	let manifest_dir = env!("CARGO_MANIFEST_DIR");
-	let crate_path = [manifest_dir, "..", "engine"]
+	let crate_path = [manifest_dir, "..", "shader"]
 		.iter()
 		.copied()
 		.collect::<PathBuf>();
