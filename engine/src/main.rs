8442c2e3febefd7c4465de1135f294c366b94b24
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2023-01-19 20:03:07 +0100

    broken: compiling engine for both cpu and spirv

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,20 +1,7 @@
 use std::error::Error;
-use std::path::PathBuf;
+use engine::management;
 
-use spirv_builder::{MetadataPrintout, SpirvBuilder};
-
+#[cfg(not(target_arch = "spirv"))]
 fn main() -> Result<(), Box<dyn Error>> {
-	let manifest_dir = env!("CARGO_MANIFEST_DIR");
-	let crate_path = [manifest_dir, "..", "shader"]
-		.iter()
-		.copied()
-		.collect::<PathBuf>();
-	let result = SpirvBuilder::new(crate_path, "spirv-unknown-vulkan1.1")
-		.print_metadata(MetadataPrintout::Full)
-		.build()?;
-
-	let module_path = result.module.unwrap_single();
-	println!("module_path: {}", module_path.to_str().unwrap());
-	println!("entry points: {:?}", result.entry_points);
-	Ok(())
+	management::main()
 }
